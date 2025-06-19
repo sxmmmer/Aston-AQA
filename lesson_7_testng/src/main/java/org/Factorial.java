438e46209462543e5f60f12c1aa5e16b198f2d14
sxmmmer <alexsommer2003@gmail.com>
2025-06-19 16:40:35 +0300

    lesson_7_testng

---
+++ lesson_7_testng/src/main/java/org/Factorial.java
@@ -0,0 +1,11 @@
+package org;
+
+public class Factorial {
+    public static int factorial(int number) {
+        int result = 1;
+        for (int i = 1; i <= number; i++) {
+            result = result * i;
+        }
+        return result;
+    }
+}
